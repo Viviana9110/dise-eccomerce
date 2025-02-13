43bfe666e121f1318b88becb45fb47b20712e1fe
Viviana9110 <v.londono.naranjo@gmail.com>
2025-02-12 20:37:22 -0500

    modifique la conexion al backend

--- frontend/src/components/CheckoutForm.jsx
+++ frontend/src/components/CheckoutForm.jsx
@@ -14,7 +14,7 @@ function CheckoutForm() {
     e.preventDefault();
     
     try {
-      const response = await fetch('https://dise-eccomerce.onrender.com/api/send-order', {
+      const response = await fetch('https://dise-eccomerceback.onrender.com/api/send-order', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
